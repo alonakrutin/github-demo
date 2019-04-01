b139b4ac47101da69b9f25325f0c7a2c2a824b4d
Alona Krutin <alonka.krutin@gmail.com>
2019-04-01 10:03:44 +0300

    New file Season Enum

---
+++ src/EnjoySeason.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EnjoySeason {
+
+   public enum Season {
+        WINTER, SPRING, SUMMER, AUTUMN
+    }
+
+    public static void main(String[ ] args) throws IOException {
+        Season season;
+
+        BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+        System.out.println("Enter month");
+        String month = br.readLine();
+
+        switch (month) {
+            case "December":
+            case "January":
+            case "February":
+                season = Season.WINTER;
+                System.out.println(season);
+                break;
+            case "March":
+            case "April":
+            case "May":
+                season = Season.SPRING;
+                System.out.println(season);
+                break;
+            case "June":
+            case "Jule":
+            case "August":
+                season = Season.SUMMER;
+                System.out.println(season);
+                break;
+            case "September":
+            case "October":
+            case "November":
+                season = Season.AUTUMN;
+                System.out.println(season);
+                break;
+            default:
+                System.out.println("No this month");
+        }
+
+
+
+
+    }
+
+
+}
+
